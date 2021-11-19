3d69593539a28dfddff97c42d4884f0ed302a391
Rodger Retanal <rodger.retanal24@gmail.com>
2021-11-19 13:03:02 -0500

    [feature] CoverImage element
    * Styles for object-fit cover

---
+++ elements/ImageCover.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import ResponsiveImage from '../components/ResponsiveImage';
+
+export default styled(ResponsiveImage)`
+	width: 100%;
+	object-fit: cover;
+`;
