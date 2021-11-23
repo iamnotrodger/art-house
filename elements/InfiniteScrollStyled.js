6cca910c9c25acfa8a8d0f41d9b5d4de2820a193
Rodger Retanal <rodger.retanal24@gmail.com>
2021-11-23 16:25:23 -0500

    [feature] InifniteScroll styled component

---
+++ elements/InfiniteScrollStyled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { size } from '../utils/breakpoints';
+
+export default styled(InfiniteScroll)`
+	max-width: ${(props) => (props.width ? props.width : size['3xl'])}px;
+`;
--- utils/breakpoints.js
+++ utils/breakpoints.js
@@ -4,6 +4,7 @@ export const size = {
 	lg: 1024,
 	xl: 1280,
 	'2xl': 1536,
+	'3xl': 1680,
 };
 
 const breakpoints = {
@@ -12,6 +13,7 @@ const breakpoints = {
 	lg: `(min-width: ${size.lg}px)`,
 	xl: `(min-width: ${size.xl}px)`,
 	'2xl': `(min-width: ${size['2xl']}px)`,
+	'3xl': `(min-width: ${size['3xl']}px)`,
 };
 
 export default breakpoints;
