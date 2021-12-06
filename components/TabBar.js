76f11b5c05ddf1189290dc56dd60264ac2e70794
Rodger Retanal <rodger.retanal24@gmail.com>
2021-12-06 18:39:01 -0500

    [feature] TabBar component

---
+++ components/TabBar.js
@@ -0,0 +1,53 @@
+import { useRouter } from 'next/router';
+import styled, { css } from 'styled-components';
+import HomeIcon from '../icons/HomeIcon';
+import SearchIcon from '../icons/SearchIcon';
+import breakpoints from '../utils/breakpoints';
+import NavLink from './NavLink';
+
+const TabBar = ({ active, className }) => {
+	const router = useRouter();
+
+	return (
+		<Bar active={active} className={className}>
+			<NavLink to='/' active={'/' === router.pathname}>
+				<HomeIcon height='1' width='1' />
+			</NavLink>
+			<NavLink to='/search' active={'/search' === router.pathname}>
+				<SearchIcon height='1' width='1' />
+			</NavLink>
+		</Bar>
+	);
+};
+
+const Bar = styled.div`
+	display: flex;
+	position: fixed;
+	z-index: 50;
+	gap: 0.5rem;
+	bottom: 1rem;
+	left: 50%;
+	padding: 0.5rem 1rem;
+	background: var(--color-light);
+	border-radius: var(--rounded-full);
+	box-shadow: var(--shadow-md);
+	transition: var(--transition);
+	transition-duration: 250ms;
+
+	${(props) =>
+		props.active
+			? css`
+					opacity: 1;
+					transform: translate(-50%, 0);
+			  `
+			: css`
+					opacity: 0;
+					transform: translate(-50%, 175%);
+			  `}
+
+	@media ${breakpoints.md} {
+		display: none;
+	}
+`;
+
+export default TabBar;
