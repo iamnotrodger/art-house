ef6153c99c2bf009a636d745b021a0d1e3f81180
Rodger Retanal <rodger.retanal24@gmail.com>
2021-12-07 14:26:21 -0500

    [feature] Added React Query hydration

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,27 @@
 import 'gestalt/dist/gestalt.css';
-import { Fragment } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { Fragment, useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import NavLayout from '../components/NavLayout';
 import { SearchProvider } from '../contexts/SearchContext';
 import { WindowSizeProvider } from '../contexts/WindowSizeContext';
 import Main from '../elements/Main';
 import '../styles/globals.css';
 
-const queryClient = new QueryClient();
-
 const MyApp = ({ Component, pageProps }) => {
+	const [queryClient] = useState(() => new QueryClient());
 	return (
 		<Fragment>
 			<QueryClientProvider client={queryClient}>
-				<WindowSizeProvider>
-					<SearchProvider>
-						<NavLayout />
-						<Main>
-							<Component {...pageProps} />
-						</Main>
-					</SearchProvider>
-				</WindowSizeProvider>
+				<Hydrate state={pageProps.dehydratedState}>
+					<WindowSizeProvider>
+						<SearchProvider>
+							<NavLayout />
+							<Main>
+								<Component {...pageProps} />
+							</Main>
+						</SearchProvider>
+					</WindowSizeProvider>
+				</Hydrate>
 			</QueryClientProvider>
 		</Fragment>
 	);
