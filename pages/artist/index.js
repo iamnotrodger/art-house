d268236c1a31d8a9ea00a65c4a6ae902a9b14424
Rodger Retanal <rodger.retanal24@gmail.com>
2021-12-07 14:27:25 -0500

    [update] Added React-Query hydration to artist explore page

--- pages/artist/index.js
+++ pages/artist/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { Fragment, useEffect, useRef, useState } from 'react';
-import { useInfiniteQuery } from 'react-query';
+import { dehydrate, QueryClient, useInfiniteQuery } from 'react-query';
 import { getArtists } from '../../api/ArtistAPI';
 import ArtistList from '../../components/ArtistList';
 import Title from '../../elements/Title';
@@ -9,37 +9,37 @@ import { parseError } from '../../utils/error';
 import Error from '../_error';
 
 let cursor = 0;
-const defaultArtistsLimit = 20;
+const limit = process.env.NEXT_PUBLIC_ARTISTS_LIMIT || 20;
 
-const ArtistExplorePage = ({ value, error }) => {
+const ArtistExplorePage = ({ error }) => {
 	const fetchArtists = ({ pageParam = 0 }) => {
 		cursor += pageParam;
 		return getArtists({
 			skip: cursor,
-			limit: process.env.NEXT_PUBLIC_ARTISTS_LIMIT || defaultArtistsLimit,
+			limit,
 		});
 	};
 
 	const endScrollRef = useRef();
 	const [errorState, setErrorState] = useState(error);
-	const { data, hasNextPage, fetchNextPage, ...query } = useInfiniteQuery(
-		'artists',
-		fetchArtists,
-		{
-			getNextPageParam: (lastPage, pages) => {
-				if (lastPage.length > 0) return lastPage.length;
-				return undefined;
-			},
-			initialData: { pages: [value] },
-			enabled: false,
-		}
-	);
+	const {
+		data,
+		hasNextPage,
+		fetchNextPage,
+		error: queryError,
+	} = useInfiniteQuery('artists', fetchArtists, {
+		getNextPageParam: (lastPage, pages) => {
+			if (lastPage.length > 0) return lastPage.length;
+			return undefined;
+		},
+		enabled: false,
+	});
 
 	useEffect(() => {
-		if (query.error) {
-			setErrorState(query.error);
+		if (queryError) {
+			setErrorState(queryError);
 		}
-	}, [query.error]);
+	}, [queryError]);
 
 	useIntersectionObserver({
 		target: endScrollRef,
@@ -69,15 +69,17 @@ export const getServerSideProps = async ({ req, res }) => {
 		'public, s-maxage=60, stale-while-revalidate=59'
 	);
 
+	const queryClient = new QueryClient();
+
 	try {
-		const artists = await getArtists({
-			limit: process.env.NEXT_PUBLIC_ARTISTS_LIMIT || defaultArtistsLimit,
+		await queryClient.prefetchQuery('artists', async () => {
+			const artists = await getArtists({ limit });
+			return {
+				pages: [artists],
+			};
 		});
-		return {
-			props: {
-				value: artists,
-			},
-		};
+
+		return { props: { dehydratedState: dehydrate(queryClient) } };
 	} catch (error) {
 		return {
 			props: {
