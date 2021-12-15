7e987e0adfbeab8defa1bd9cd5a75ab2ef9b0a0d
iamnotrodger <rodger.retanal24@gmail.com>
2021-12-15 15:30:42 -0500

    [update] parseError default message value

--- utils/error.js
+++ utils/error.js
@@ -1,6 +1,6 @@
 export const parseError = (error) => {
 	return {
 		statusCode: error.statusCode || 500,
-		message: error.message || null,
+		message: error.message || 'Oh oh! Something went wrong.',
 	};
 };
