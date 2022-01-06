51f5000bdeb8eb264a0d464f6275e548fe6f17df
iamnotrodger <rodger.retanal24@gmail.com>
2022-01-06 12:44:01 -0500

    [update] Artist Explore page added state time and removed unccessary error prop

--- pages/artist/index.js
+++ pages/artist/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Fragment, useEffect, useRef, useState } from 'react';
+import { Fragment, useRef } from 'react';
 import { dehydrate, QueryClient, useInfiniteQuery } from 'react-query';
 import { getArtists } from '../../api/ArtistAPI';
 import ArtistList from '../../components/ArtistList';
@@ -11,7 +11,7 @@ import Error from '../_error';
 let cursor = 0;
 const limit = process.env.NEXT_PUBLIC_ARTISTS_LIMIT || 20;
 
-const ArtistExplorePage = ({ error }) => {
+const ArtistExplorePage = () => {
 	const fetchArtists = ({ pageParam = 0 }) => {
 		cursor += pageParam;
 		return getArtists({
@@ -21,25 +21,18 @@ const ArtistExplorePage = ({ error }) => {
 	};
 
 	const endScrollRef = useRef();
-	const [errorState, setErrorState] = useState(error);
-	const {
-		data,
-		hasNextPage,
-		fetchNextPage,
-		error: queryError,
-	} = useInfiniteQuery('artists', fetchArtists, {
-		getNextPageParam: (lastPage, pages) => {
-			if (lastPage.length > 0) return lastPage.length;
-			return undefined;
-		},
-		enabled: false,
-	});
-
-	useEffect(() => {
-		if (queryError) {
-			setErrorState(queryError);
+	const { data, error, hasNextPage, fetchNextPage } = useInfiniteQuery(
+		'artists',
+		fetchArtists,
+		{
+			getNextPageParam: (lastPage, pages) => {
+				if (lastPage.length > 0) return lastPage.length;
+				return undefined;
+			},
+			enabled: false,
+			staleTime: 60 * 1000,
 		}
-	}, [queryError]);
+	);
 
 	useIntersectionObserver({
 		target: endScrollRef,
@@ -47,46 +40,29 @@ const ArtistExplorePage = ({ error }) => {
 		enabled: hasNextPage,
 	});
 
-	if (errorState) return <Error {...errorState} />;
-
-	const artists = data.pages.flat();
-
+	if (error) return <Error {...parseError(error)} />;
 	return (
 		<Fragment>
 			<Head>
 				<title>Explore Artists</title>
 			</Head>
 			<Title>Artists</Title>
-			<ArtistList items={artists} />
+			<ArtistList items={data.pages.flat()} />
 			<div ref={endScrollRef}></div>
 		</Fragment>
 	);
 };
 
-export const getServerSideProps = async ({ req, res }) => {
-	res.setHeader(
-		'Cache-Control',
-		'public, s-maxage=60, stale-while-revalidate=59'
-	);
-
+export const getServerSideProps = async () => {
 	const queryClient = new QueryClient();
 
-	try {
-		await queryClient.prefetchQuery('artists', async () => {
-			const artists = await getArtists({ limit });
-			return {
-				pages: [artists],
-			};
-		});
-
-		return { props: { dehydratedState: dehydrate(queryClient) } };
-	} catch (error) {
+	await queryClient.prefetchQuery('artists', async () => {
+		const artists = await getArtists({ limit });
 		return {
-			props: {
-				error: parseError(error),
-			},
+			pages: [artists],
 		};
-	}
+	});
+	return { props: { dehydratedState: dehydrate(queryClient) } };
 };
 
 export default ArtistExplorePage;
