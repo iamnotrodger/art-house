a0cead2602e195a78bae4be877da63fad285f2d5
iamnotrodger <rodger.retanal24@gmail.com>
2022-01-07 16:33:37 -0500

    [update] Title element style

--- elements/Title.js
+++ elements/Title.js
@@ -4,12 +4,9 @@ import breakpoints from '../utils/breakpoints';
 export default styled.h1`
 	font-size: var(--text-2xl);
 	font-weight: var(--font-bold);
+	line-height: 1.1;
 
 	@media ${breakpoints.md} {
 		font-size: var(--text-6xl);
-	}
-
-	@media ${breakpoints.lg} {
-		font-size: var(--text-8xl);
 	}
 `;
